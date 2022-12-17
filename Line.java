deb63ab5ecf640c3b15451460f7e7f0773a60d1e
amin-mlm <aminmoslem77@gmail.com>
2022-12-17 11:38:26 +0330

    lineNumber is added to the class "Line" to be applicable when find() method is invoked.

--- Line.java
+++ Line.java
@@ -1,10 +1,12 @@
 public class Line implements Cloneable{
     String text;
     Line nextLine;
+    int lineNumber;
 
-    public Line(String str){
+    public Line(String str, int lineNumber){
         text = str;
         nextLine = null;
+        this.lineNumber = lineNumber;
     }
 
     public Line(Line a){
